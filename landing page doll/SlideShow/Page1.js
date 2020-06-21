e2f775970d588f8f913ef1f7915767354ef02ce2
minhnt303 <minhnt303@gmail.com>
2020-06-21 23:24:40 +0700

    Commit to github

---
+++ landing page doll/SlideShow/Page1.js
@@ -0,0 +1,39 @@
+
+
+
+
+function changePage() {
+    var idPage = $("#idPage").val();
+    console.log(idPage);
+    if(idPage == 1){
+        window.location.href = '../Desktop24/index.html';
+    }
+    else if(idPage == 2){
+        window.location.href = '../DesktopBst/index.html';
+    }
+    else if(idPage == 3){
+        window.location.href = '../iMac16/Page1.html';
+    }
+    else if(idPage == 4){
+        window.location.href = '../iMac17/Page1.html';
+    }
+    else if(idPage == 5){
+        window.location.href = '../iMac18/Page1.html';
+    }
+    else if(idPage == 6){
+        window.location.href = '../iMac19/Page1.html';
+    }
+    else if(idPage == 7){
+        window.location.href = '../iMac20/Page1.html';
+    }
+    else if(idPage == 8){
+        window.location.href = '../iMac21/Page1.html';
+    }
+    else if(idPage == 9){
+        window.location.href = '../iMac22/Page1.html';
+    }
+    else if(idPage == 10){
+        window.location.href = '../iMac23/Page1.html';
+    }
+}
+// setTimeout(changePage, 5000);
